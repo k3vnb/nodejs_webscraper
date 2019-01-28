1cd242a0405e93f778192c5fcc79af671e26ed79
Kevin Boyle <papershack@gmail.com>
2019-01-27 21:36:25 -0800

    setting up amazon scraper

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const puppeteer = require('puppeteer');
-const twitter = require('./twitter');
 
 (async () => {
-
-    const USERNAME = 'fakeusername';
-    const PASSWORD = 'fakepassword';
-    await twitter.initialize();
-
-    await twitter.login(USERNAME, PASSWORD);
-
-
-
-    debugger;
+    const browser = await puppeteer.launch({ 
+        headless: false
+        // ,
+        // args: ['--proxy-server=45.125.222.9:8080']
+    });
+    const page = await browser.newPage();
+  
+    await page.goto('https://amazon.com')
+    await page.type('.search-key-box input', 'tent', {delay: 10})
+    await page.keyboard.press('Enter');
+    await page.waitForNavigation();
+    const link = await page.url();
+    console.log(link);
     // await browser.close();
 
 })();
